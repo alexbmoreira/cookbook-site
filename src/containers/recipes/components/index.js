f68925791fc9486772d455ab6d4ce7b1d93c1c54
Alex Moreira <alexbmoreira@gmail.com>
2022-10-09 15:04:44 -0400

    Restructure folders and imports (#3)

---
+++ src/containers/recipes/components/index.js
@@ -0,0 +1,5 @@
+import RecipesContainer from './RecipesContainer';
+
+export {
+  RecipesContainer
+};
---
+++ src/containers/recipes/index.js
@@ -0,0 +1,5 @@
+import { RecipesContainer } from './components';
+
+export {
+  RecipesContainer
+};
