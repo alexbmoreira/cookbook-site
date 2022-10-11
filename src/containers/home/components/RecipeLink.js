bb3de9c29c55916bd9b64048be5e75c1b8599db0
Alex Moreira <alexbmoreira@gmail.com>
2022-10-10 23:30:18 -0400

    redesign list elements (#10)

--- src/containers/home/components/RecipeLink.js
+++ src/containers/home/components/RecipeLink.js
@@ -4,22 +4,22 @@ import { FormattedMessage } from 'react-intl';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Card = ({children}) => {
-  return (<div className='drop-shadow bg-powder active:drop-shadow-none'>
+  return (<div className='drop-shadow bg-carolina active:drop-shadow-none'>
     {children}
   </div>);
 };
 
 const RecipeInfo = ({recipe}) => {
   return (
-    <div className='p-2'>
+    <div className='p-2 text-white'>
       <div className='font-serif text-2xl mb-1'>
         {recipe.name}
       </div>
-      <div className='text-lapis flex justify-between'>
+      <div className='flex justify-between'>
         <div>
           <FormattedMessage id={`recipes.categories.${recipe.category}`}/>
         </div>
-        <div className='space-x-1 text-manatee text-sm'>
+        <div className='space-x-1 opacity-50 text-sm'>
           <FontAwesomeIcon icon="fa-solid fa-clock"/>
           <span>
             <FormattedMessage id='time.minutes' values={{time: recipe.prepTime + recipe.cookTime}}/>
