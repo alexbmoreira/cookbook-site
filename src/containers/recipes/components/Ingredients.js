318f5ed46f8fce529edccec745a2aee8514619c9
Alex Moreira <alexbmoreira@gmail.com>
2022-10-10 23:16:25 -0400

    set up api with some real recipes

--- src/containers/recipes/components/Ingredients.js
+++ src/containers/recipes/components/Ingredients.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 const Ingredient = ({name, quantity, measurement}) => {
   return (
-    <div>
-      <span className='bold text-lapis'>{`${quantity} ${measurement} `}</span>
-      <span className='font-serif'>{name}</span>
-    </div>
+    <div><span className='font-serif'>{`${quantity} ${measurement} ${name}`}</span></div>
   )
 }
 
 const Ingredients = ({ingredients}) => {
   if (!ingredients) return null;
 
-  return ingredients.map((ingredient, index) => (
-    <div className='space-y-1 text-sm'>
-      <Ingredient key={index} name={ingredient.name} quantity={ingredient.quantity} measurement={ingredient.measurement}/>
+  return (
+    <div className='text-sm'>
+      {ingredients.map((ingredient, index) => (
+        <Ingredient key={index} name={ingredient.name} quantity={ingredient.quantity} measurement={ingredient.measurement}/>
+      ))}
     </div>
-  ));
+  );
 };
 
 export default Ingredients;
