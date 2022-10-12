393f13f2fccb1640a6c690716db0df9df6a6c0fa
Alex Moreira <alexbmoreira@gmail.com>
2022-10-12 19:05:42 -0400

    Add more recipes (#22)

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,15 +1,17 @@
 import _ from 'lodash';
 
 const recipes = [
-  _.merge({ id: "1" }, require('./catalog/breakfast/chocolate_chip_pancakes')),
-  _.merge({ id: "2" }, require('./catalog/breakfast/french_toast')),
-  _.merge({ id: "3" }, require('./catalog/breakfast/hash_browns')),
-  _.merge({ id: "4" }, require('./catalog/breakfast/scrambled_eggs')),
-  _.merge({ id: "7" }, require('./catalog/dessert/creme_brulee')),
-  _.merge({ id: "8" }, require('./catalog/meat_and_poultry/chicken_yakitori_bowl')),
-  _.merge({ id: "9" }, require('./catalog/meat_and_poultry/pan_seared_steak')),
-  _.merge({ id: "6" }, require('./catalog/side/sweet_potato_casserole')),
-  _.merge({ id: "5" }, require('./catalog/soup/caldo_verde')),
+  _.merge({ id: "1" },  require('./catalog/breakfast/chocolate_chip_pancakes')),
+  _.merge({ id: "2" },  require('./catalog/breakfast/french_toast')),
+  _.merge({ id: "3" },  require('./catalog/breakfast/hash_browns')),
+  _.merge({ id: "4" },  require('./catalog/breakfast/scrambled_eggs')),
+  _.merge({ id: "5" },  require('./catalog/soup/caldo_verde')),
+  _.merge({ id: "6" },  require('./catalog/side/sweet_potato_casserole')),
+  _.merge({ id: "11" }, require('./catalog/side/mashed_potatoes')),
+  _.merge({ id: "8" },  require('./catalog/meat_and_poultry/chicken_yakitori_bowl')),
+  _.merge({ id: "9" },  require('./catalog/meat_and_poultry/pan_seared_steak')),
+  _.merge({ id: "7" },  require('./catalog/dessert/creme_brulee')),
+  _.merge({ id: "10" }, require('./catalog/dessert/banana_bread')),
 ]
 
 if (_.uniqBy(recipes, 'id').length !== recipes.length) {
