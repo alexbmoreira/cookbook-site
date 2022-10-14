2eef4eca3ba7e93a582cd43b507a0db434a3d82a
Alex Moreira <alexbmoreira@gmail.com>
2022-10-13 22:56:57 -0400

    wip (#26)

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,18 +1,21 @@
 import _ from 'lodash';
 
 const recipes = [
-  _.merge({ id: "1" },  require('./catalog/breakfast/chocolate_chip_pancakes')),
-  _.merge({ id: "2" },  require('./catalog/breakfast/french_toast')),
-  _.merge({ id: "3" },  require('./catalog/breakfast/hash_browns')),
-  _.merge({ id: "4" },  require('./catalog/breakfast/scrambled_eggs')),
-  _.merge({ id: "5" },  require('./catalog/soup/caldo_verde')),
-  _.merge({ id: "6" },  require('./catalog/side/garlic_roasted_potatoes')),
-  _.merge({ id: "7" },  require('./catalog/side/sweet_potato_casserole')),
-  _.merge({ id: "8" },  require('./catalog/side/mashed_potatoes')),
-  _.merge({ id: "9" },  require('./catalog/meat_and_poultry/chicken_yakitori_bowl')),
-  _.merge({ id: "10" }, require('./catalog/meat_and_poultry/pan_seared_steak')),
-  _.merge({ id: "11" }, require('./catalog/dessert/creme_brulee')),
-  _.merge({ id: "12" }, require('./catalog/dessert/banana_bread')),
+  _.merge({ id: "1" },  require('./catalog/meat_and_poultry/almond_crusted_chicken')),
+  _.merge({ id: "2" },  require('./catalog/fish/maple_dijon_salmon')),
+  _.merge({ id: "3" },  require('./catalog/meat_and_poultry/pan_seared_steak')),
+  _.merge({ id: "4" },  require('./catalog/soup/caldo_verde')),
+  _.merge({ id: "5" },  require('./catalog/side/garlic_roasted_potatoes')),
+  _.merge({ id: "6" },  require('./catalog/dessert/banana_bread')),
+  _.merge({ id: "7" },  require('./catalog/breakfast/chocolate_chip_pancakes')),
+  _.merge({ id: "8" },  require('./catalog/side/sweet_potato_casserole')),
+  _.merge({ id: "9" },  require('./catalog/breakfast/scrambled_eggs')),
+  _.merge({ id: "10" }, require('./catalog/breakfast/hash_browns')),
+  _.merge({ id: "11" }, require('./catalog/side/mashed_potatoes')),
+  _.merge({ id: "12" }, require('./catalog/breakfast/french_toast')),
+  _.merge({ id: "13" }, require('./catalog/meat_and_poultry/chicken_yakitori_bowl')),
+  _.merge({ id: "14" }, require('./catalog/dessert/creme_brulee')),
+  _.merge({ id: "15" }, require('./catalog/pasta/pasta_carbonara')),
 ]
 
 if (_.uniqBy(recipes, 'id').length !== recipes.length) {
