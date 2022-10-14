1391e3649686b1d67dc4b0708c70dacbee38fccd
Alex Moreira <alexbmoreira@gmail.com>
2022-10-14 19:02:09 -0400

    Sort recipes (#31)
    And clean up imports

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,28 +1,12 @@
-var _ = require('lodash');
-var slugify = require('slugify');
+const _ = require('lodash');
+const slugify = require('slugify');
 
-var recipes = [
-  require('./catalog/meat_and_poultry/almond_crusted_chicken'),
-  require('./catalog/fish/maple_dijon_salmon'),
-  require('./catalog/meat_and_poultry/pan_seared_steak'),
-  require('./catalog/soup/caldo_verde'),
-  require('./catalog/side/garlic_roasted_potatoes'),
-  require('./catalog/dessert/banana_bread'),
-  require('./catalog/breakfast/chocolate_chip_pancakes'),
-  require('./catalog/side/sweet_potato_casserole'),
-  require('./catalog/breakfast/scrambled_eggs'),
-  require('./catalog/breakfast/hash_browns'),
-  require('./catalog/side/mashed_potatoes'),
-  require('./catalog/breakfast/french_toast'),
-  require('./catalog/meat_and_poultry/chicken_yakitori_bowl'),
-  require('./catalog/dessert/creme_brulee'),
-  require('./catalog/pasta/pasta_carbonara'),
-  require('./catalog/dessert/famous_pumpkin_pie.json'),
-  require('./catalog/pasta/macs_famous_mac_and_cheese.json'),
-  require('./catalog/dessert/chocolate_chunk_cookies.json'),
-]
+const requireModule = require.context('./catalog/', true, /\.json$/);
 
-recipes = _.map(recipes, (r) => _.merge({ slug: slugify(r.name, { lower: true, remove: /[*+~.()'"!:@]/g }) }, r));
+const recipes = requireModule.keys().map(filename => {
+  const recipe = require(`./catalog/${filename.replace(/(\.\/|\.json)/g, '')}`);
+  return _.merge({ slug: slugify(recipe.name, { lower: true, remove: /[*+~.()'"!:@]/g }) }, recipe);
+});
 
 if (_.uniqBy(recipes, 'slug').length !== recipes.length) {
   throw new Error('Duplicate ids in recipes');
