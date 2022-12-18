ce5ccc0632cfb6d64660befacf09457f01355c2d
Alex Moreira <alexbmoreira@gmail.com>
2022-12-17 22:21:50 -0500

    Add images (#37)

--- src/containers/recipes/components/RecipesContainer.js
+++ src/containers/recipes/components/RecipesContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from '../../../components';
 import BlockHeader from './BlockHeader';
-import { withState } from '../../../shared';
+import { imageUrl, withState } from '../../../shared';
 import RecipesContainerState from '../state/RecipesContainerState';
 import RecipeInfo from './RecipeInfo';
 import Ingredients from './Ingredients';
@@ -12,8 +12,11 @@ const RecipesContainer = ({uiState}) => {
 
   return (
     <div>
-      {recipe.image && <div className='h-48 bg-gray-600'/>}
-      <Container className='space-y-2'>
+      {recipe.image && <div
+        className='bg-cover bg-center h-48 overflow-hidden bg-gray-600'
+        style={{backgroundImage: `url(${imageUrl(recipe.category, recipe.image)})`}}
+      />}
+      <Container className='space-y-2 z-50'>
         <BlockHeader title={recipe.name} size='lg' translateTitle={false}/>
         <RecipeInfo recipe={recipe}/>
         <BlockHeader title={'recipes.Ingredients'}/>
