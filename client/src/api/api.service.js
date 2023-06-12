b0861c0ba72a40fd5c29db1866c46217e15a5e50
Alex Moreira <alexbmoreira@gmail.com>
2023-06-12 01:24:19 -0400

    remove logs

--- client/src/api/api.service.js
+++ client/src/api/api.service.js
@@ -4,7 +4,6 @@ const api = axios.create();
 
 export const fetchData = async (url) => {
   try {
-    console.log(process.env.REACT_APP_API_URL);
     const response = await api.get(`${process.env.REACT_APP_API_URL}${url}`);
     return response.data;
   } catch (error) {
