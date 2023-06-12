c86b2f0a99f3a2db19357236e165630b9eee9272
Alex Moreira <alexbmoreira@gmail.com>
2023-06-11 20:45:13 -0400

    Recipes backend (#44)
    * recipe models
    
    * wip
    
    * recipes show
    
    * fix
    
    * git ignore
    
    * heroku

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class RecipesController < ApplicationController
+      def show
+        render json: Recipe.find(params[:id]), except: [:deleted_at, :created_at, :updated_at], status: :ok
+      end
+
+      def index
+        category = params[:category].to_sym
+        render json: Recipe.send(category), except: [:deleted_at, :created_at, :updated_at], status: :ok
+      end
+    end
+  end
+end
---
+++ app/models/recipe_ingredient.rb
@@ -0,0 +1,29 @@
+class RecipeIngredient < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :ingredient
+end
+
+# == Schema Information
+#
+# Table name: recipe_ingredients
+#
+#  id            :bigint           not null, primary key
+#  deleted_at    :datetime
+#  measurement   :string
+#  quantity      :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  ingredient_id :bigint           not null
+#  recipe_id     :bigint           not null
+#
+# Indexes
+#
+#  index_recipe_ingredients_on_deleted_at     (deleted_at)
+#  index_recipe_ingredients_on_ingredient_id  (ingredient_id)
+#  index_recipe_ingredients_on_recipe_id      (recipe_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ingredient_id => ingredients.id)
+#  fk_rails_...  (recipe_id => recipes.id)
+#
