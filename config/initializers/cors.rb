c76b8a7daefa3475917e3d5f51b3d7e9aa48c33d
Alex Moreira <alexbmoreira@gmail.com>
2023-06-15 01:08:11 -0400

    hotfix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,22 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins /twoscompanycookbook\.com/
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins /cookbook-13--[-\w]+\.web\.app/
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
