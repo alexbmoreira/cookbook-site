dae2a8079480dabbecb9e4a09a7a2bc82874d457
Alex Moreira <alexbmoreira@gmail.com>
2023-06-16 19:25:48 -0400

    Small changes (#58)
    * Small changes
    
    * wip

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -6,16 +6,16 @@ const Input = observer(({value, onChange, iconClass, errorMessage, ...rest}) => 
   return (
     <div className='relative mb-2'>
       {iconClass && <span className='absolute text-silver left-2 bottom-2'>
-        <FontAwesomeIcon icon={iconClass} />
+        <FontAwesomeIcon icon={iconClass}/>
       </span>}
       <input
         {...rest}
         value={value}
-        className={`w-full border-silver border rounded p-2 focus:outline-0 ${iconClass ? 'pl-8' : ''} ${errorMessage ? 'border-crimson' : ''}`}
+        className={`w-full border-silver placeholder:text-silver border rounded p-2 focus:outline-0 ${iconClass ? 'pl-8' : ''} ${errorMessage ? 'border-crimson' : ''}`}
         onChange={(e) => onChange(e.target.value)}
       />
       {errorMessage && <div className='text-crimson text-xs space-x-1 mt-1'>
-        <FontAwesomeIcon icon='fa-solid fa-circle-exclamation' />
+        <FontAwesomeIcon icon='fa-solid fa-circle-exclamation'/>
         <span>{errorMessage}</span>
       </div>}
     </div>
