127e57373fa675d035d20b0f1189695d8b36081e
Alex Moreira <alexbmoreira@gmail.com>
2023-06-19 18:41:10 -0400

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'google/cloud/storage'
 
 ActiveRecord::Base.transaction do
-  debugger
+  p Rails.application.credentials[Rails.env.to_sym][:google_cloud_keyfile]
+  p Base64.decode64(Rails.application.credentials[Rails.env.to_sym][:google_cloud_keyfile])
+
   storage = Google::Cloud::Storage.new(
     project_id: 'dazzling-rain-372001',
     credentials: JSON.parse(Base64.decode64(
