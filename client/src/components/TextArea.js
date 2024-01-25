a288d5d6a314ff8143f4d407eddcc3a5d31cc290
Alex Moreira <alexbmoreira@gmail.com>
2024-01-25 15:18:45 -0500

    Text Editor

--- client/src/components/TextArea.js
+++ client/src/components/TextArea.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import TextareaAutosize from 'react-textarea-autosize';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FormattedMessage } from 'react-intl';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-const TextArea = observer(({value, onChange, errorMessage, ...rest}) => {
+const TextArea = observer(({value, label, onChange, errorMessage, className}) => {
+  const _onChange = (content, delta, source, editor) => {
+    onChange(editor.getHTML());
+  };
+
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],
+      ['link'],
+      ['clean']
+    ]
+  }
+
+  const formats = [
+    'bold', 'italic', 'underline',
+    'link'
+  ]
+
   return (
-    <TextareaAutosize
-      {...rest}
-      value={value}
-      className='outline-none resize-none w-full placeholder:text-silver'
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div className={className || ''}>
+      {label && <label className='block text-black-bean'>
+        <FormattedMessage id={label}/>
+      </label>}
+      <ReactQuill
+        value={value || ''}
+        theme='snow'
+        onChange={_onChange}
+        modules={modules}
+        formats={formats}
+      />
+      {errorMessage && <div className='text-crimson text-xs space-x-1 mt-1'>
+        <FontAwesomeIcon icon='fa-solid fa-circle-exclamation'/>
+        <span>{errorMessage}</span>
+      </div>}
+    </div>
   );
 });
 
